7d0b5f61c82f9ff965052d0b82cab08664387148
ChriswFoster <chriswfoster@gmail.com>
2018-02-28 20:53:48 -0600

    Set proxy to 3333

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import axios from 'axios'
+import "./App.css"
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      users: []
+    }
+  }
+
+  createUser() {}
+
+  readUser() {
+    axios.get("/api/getusers")
+    .then(response => this.setState({users: response.data}))
+  }
+
+  updateUser() {}
+
+  deleteUser() {}
+
   render() {
+    const {users} = this.state
+    const userList = users.map((user, i) => <p key={i}>user</p>)
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  <div><h1>CRUDDY USERS</h1></div>
+        <div className="crudFlex">
+          <div className="crudBoxes">Create:
+          <input placeholder = "User's name" />
+          <input placeholder = "User's email" />
+          <input placeholder = "Room NUMBER" />
+          <button>Create User</button>
+          </div>
+          
+          <div className="crudBoxes">Read:
+          <button>Get List of Users</button></div>
+
+          <div className="crudBoxes">Update</div>
+          <div className="crudBoxes">Delete</div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
