1c80350e033e4ee369411383be08932983fd6233
ChriswFoster <chriswfoster@gmail.com>
2018-03-01 09:20:37 -0600

    Notes added

--- server/index.js
+++ server/index.js
@@ -14,14 +14,16 @@ const userCtrl = require(`${__dirname}/userCtrl.js`);
 const port = 3333;
 
 
+// Invoke app.use and tell it to add the middleware below. Then the middleware will be added onto your express middleware stack.
 app.use(cors());
 app.use(bodyParser.json());
 
 
-app.use((req, res, next) => {
+app.use((req, res, next) => { // we can console log our req.body, params, or query here. This will help you see if the req data is making it to the server properly. Good for troubleshooting where your requests could be failing, and to see what your data looks like on the server side.
     console.log("Middleware firing off...")
     console.log("Body: ", req.body)
     console.log("Query: ", req.query)
+    // invoke next to pass the request through...
   next();
 });
 
