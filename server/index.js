ad8fbe65adc56c8a491260bed08dbba64c1e6939
ChriswFoster <chriswfoster@gmail.com>
2018-02-28 22:55:29 -0600

    Preparing node for review

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
+// we need to require express, body-parser, and cors.
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+//assign express to our app variable
 const app = express();
 
+// import controller that holds userdata and methods
 const userCtrl = require(`${__dirname}/userCtrl.js`);
 
+//Define port to listen to
 const port = 3333;
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 
-
+//define methods, paths, and handlers.
 app.get("/api/getusers", userCtrl.getUsers)
 app.post("/api/createUser", userCtrl.createUser)
 app.put("/api/updateuser", userCtrl.updateUser)
